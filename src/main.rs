d6c1cc1d29f7ce37b77c4ffab3c4dba2e95c1db4
Mika Fischer <mika.fischer@zoopnet.de>
2022-08-08 21:35:04 +0200

    Better error handling, more clippy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::io::{self, Read, Write};
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use std::io::{Read, Write};
 use std::net::{TcpStream, ToSocketAddrs};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
 use clap::{self, Parser};
-use humantime;
+use color_eyre::eyre;
 use rustls::{Certificate, ClientConfig, ClientConnection, RootCertStore, Stream};
 use rustls_native_certs::load_native_certs;
 
@@ -21,15 +23,15 @@ struct Args {
     connect_timeout: humantime::Duration,
 }
 
-fn get_root_store() -> Result<RootCertStore, io::Error> {
+fn get_root_store() -> Result<RootCertStore, eyre::Report> {
     let mut root_store = RootCertStore::empty();
     for cert in load_native_certs()? {
-        root_store.add(&Certificate(cert.0)).unwrap();
+        root_store.add(&Certificate(cert.0))?;
     }
     Ok(root_store)
 }
 
-fn get_client_config() -> Result<Arc<ClientConfig>, io::Error> {
+fn get_client_config() -> Result<Arc<ClientConfig>, eyre::Report> {
     let config = ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(get_root_store()?)
@@ -48,12 +50,14 @@ fn get_request_for(hostname: &str) -> String {
     )
 }
 
-fn main() {
+fn main() -> Result<(), eyre::Report> {
+    color_eyre::install()?;
+
     let args = Args::parse();
 
     println!("Resolving hostname {}...", &args.hostname);
     let then = Instant::now();
-    let addrs_iter = (args.hostname.as_str(), 443).to_socket_addrs().unwrap();
+    let addrs_iter = (args.hostname.as_str(), 443).to_socket_addrs()?;
     let addrs: Vec<_> = addrs_iter.collect();
     let resolve_time = Instant::now() - then;
     for addr in &addrs {
@@ -64,37 +68,39 @@ fn main() {
         println!("Connecting to {:?}", &addr);
 
         let then = Instant::now();
-        let mut socket = TcpStream::connect_timeout(&addr, args.connect_timeout.into()).unwrap();
+        let mut socket = TcpStream::connect_timeout(&addr, args.connect_timeout.into())?;
         let connect_time = Instant::now() - then;
 
         let then = Instant::now();
-        let client_config = get_client_config().unwrap();
-        let server_name = args.hostname.as_str().try_into().unwrap();
-        let mut client = ClientConnection::new(client_config, server_name).unwrap();
-        client.complete_io(&mut socket).unwrap();
+        let client_config = get_client_config()?;
+        let server_name = args.hostname.as_str().try_into()?;
+        let mut client = ClientConnection::new(client_config, server_name)?;
+        client.complete_io(&mut socket)?;
         let handshake_time = Instant::now() - then;
 
         let then = Instant::now();
         let mut tls = Stream::new(&mut client, &mut socket);
-        tls.write_all(get_request_for(&args.hostname).as_bytes())
-            .unwrap();
+        tls.write_all(get_request_for(&args.hostname).as_bytes())?;
         let request_time = Instant::now() - then;
 
         let then = Instant::now();
         let mut plaintext = Vec::new();
-        tls.read_to_end(&mut plaintext).unwrap();
+        tls.read_to_end(&mut plaintext)?;
         let response_time = Instant::now() - then;
 
         let idx = plaintext.iter().position(|c| *c == b'\r' || *c == b'\n');
         let status_bytes = &plaintext[..idx.unwrap_or(0)];
-        let status_text = std::str::from_utf8(&status_bytes).unwrap();
+        let status_text = std::str::from_utf8(status_bytes)?;
+        let version_string = client
+            .protocol_version()
+            .map_or_else(|| "<null>".to_string(), |v| format!("{:?}", v));
+        let ciphersuite_string = client
+            .negotiated_cipher_suite()
+            .map_or_else(|| "<null>".to_string(), |v| format!("{:?}", v));
         println!(" Response status: {}", status_text);
         println!(" Response length: {:?}", plaintext.len());
-        println!(" TLS version:     {:?}", client.protocol_version().unwrap());
-        println!(
-            " Cipher suite:    {:?}",
-            client.negotiated_cipher_suite().unwrap()
-        );
+        println!(" TLS version:     {:?}", version_string);
+        println!(" Cipher suite:    {:?}", ciphersuite_string);
         let total_time =
             resolve_time + connect_time + handshake_time + request_time + response_time;
         println!(" Timings:");
@@ -105,4 +111,5 @@ fn main() {
         println!("  response:  {:?}", response_time);
         println!("  total:     {:?}", total_time);
     }
+    Ok(())
 }
